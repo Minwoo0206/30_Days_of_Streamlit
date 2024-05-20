305e88e727cbd5e35337135cf83a681dcf35fe2d
minwoo <koppie3248@gmail.com>
2024-05-21 08:09:59 +0900

    do not use `os.listdir`. instead use `Path`.

--- voting_page/voting_page.py
+++ voting_page/voting_page.py
@@ -1,17 +1,19 @@
 import os
 import csv
 import streamlit as st
+from pathlib import Path
 from PIL import Image
 
-images_dir = "./images/"
+image_folder = Path("images")
+image_files = list(image_folder.glob('*'))
 questions = []
-for idx, image_dir in enumerate(os.listdir(images_dir)):   
-    image = Image.open(images_dir+image_dir)
+for idx, image_dir in enumerate(image_files):   
+    image = Image.open(image_dir)
     st.image(image)
     question = st.slider(f"'realism' or 'illustration'?(Q{idx})", 0, 10, 5)
     questions.append(question)
 
 if st.button('submit'):
-    with open("results/result.csv", mode='a', newline='') as file:
+    with open("./results/result.csv", mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerows([questions])
