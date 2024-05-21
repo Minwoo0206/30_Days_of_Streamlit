7e238cd8635a6cea626255dc98b928e5b34c61e7
minwoo <koppie3248@gmail.com>
2024-05-21 13:56:30 +0900

    ver1.0

--- voting_page/voting_page.py
+++ voting_page/voting_page.py
@@ -1,16 +1,55 @@
+import os
 import gspread
 import pandas as pd
 import streamlit as st
-from pathlib import Path
 from PIL import Image
+from rembg import remove
 
-image_folder = Path("voting_page/images")
-image_files = list(image_folder.glob('*'))
+st.header('ACP 프로토타입 그림 선별', divider='rainbow')
+
+# 설문조사 설명 부분
+st.subheader('설명 :sunglasses:')
+st.write('''
+        여러분은 앞으로 66장의 그림을 보게 될 것이며, 그림이 실제에 가까운지(real), 애니메이션에 가까운지(animatic) 평정할 것입니다.
+        그림이 실제에 가깝다면 0점에 가깝게, 애니메이션에 가깝다면 10점에 가깝게 평정해주세요.
+        ''')
+
+try:
+    man_real = Image.open("explain/man_real.png")
+    man_anim = Image.open("explain/man_anim.png")
+except:
+    man_real = Image.open("voting_page/explain/man_real.png")
+    man_anim = Image.open("voting_page/explain/man_anim.png")
+
+st.write("0점은 왼쪽 그림을 기준으로, 10점은 오른쪽 그림을 기준으로 삼습니다.")
+col1, col2 = st.columns(2)
+with col1:
+    st.image(man_real, caption='실제', use_column_width=True)
+with col2:
+    st.image(man_anim, caption='애니메이션', use_column_width=True)
+
+st.write("이해가 다 되었다면 평정을 시작해주세요.")
+
+# 평정 부분
+st.header(body="", divider="rainbow")
+st.subheader("평정 :sunglasses:")
+
+if os.path.exists("images_processed"):
+    image_folder = "images_processed/"
+else:
+    image_folder = "voting_page/images_processed/"
+
+samples_dir =[]
+for idx, image_dir in enumerate(os.listdir(image_folder)):
+    if image_dir.split('/')[-1] == '.DS_Store': continue
+    samples_dir.append(image_folder+image_dir)
+samples_dir.sort()
+
 questions = []
-for idx, image_dir in enumerate(image_files):   
-    image = Image.open(image_dir)
-    st.image(image)
-    question = st.slider(f"'realism' or 'illustration'?(Q{idx})", 0, 10, 5)
+for idx, sample_dir in enumerate(samples_dir):
+    sample = Image.open(sample_dir)
+    st.image(sample)
+    question = st.slider(f"'real[0]' or 'animatic[10]'?(Q{idx+1})", 0, 10, 5)
     questions.append(question)
 
 # connect gc
@@ -32,9 +71,12 @@ spreadsheet_url = st.secrets['spreadsheet']
 doc = gc.open_by_url(spreadsheet_url)
 worksheet = doc.worksheet("Sheet1")
 
+# submit form
+st.header(body="", divider="rainbow")
+st.subheader("제출 :sunglasses:")
+
 name = st.text_input("이름을 입력하세요.")
 submit = st.button('submit')
-
 if submit:
     if name: 
         df = pd.DataFrame(worksheet.get_all_records())
